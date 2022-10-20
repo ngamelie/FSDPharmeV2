eab462e232473f881b0fb0f6ea6c6212400a894a
iddzjia <109298632+iddzjia@users.noreply.github.com>
2022-10-20 08:59:59 -0400

    Unit Test

---
+++ UnitTestFSDPha/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using ADFSDPhamaV2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
+using System;
+
+namespace UnitTestFSDPha
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+
+        [TestMethod]
+        public void TestGoodLogin()
+        {
+
+            ADFSDPhamaV2.Admin_User usr = new ADFSDPhamaV2.Admin_User();
+            Assert.AreEqual(true,("wu@gmail.com","123","Admin"),"should be a good user");
+
+        }
+
+    }
+}
---
+++ UnitTestFSDPha/UnitTest2.cs
@@ -0,0 +1,22 @@
+﻿using ADFSDPhamaV2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
+using System;
+
+namespace UnitTestFSDPha
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        
+
+        [TestMethod]
+        public void EqualsTest()
+        {
+           int a = 1;
+           int b = a+1;
+            Assert.AreEqual(2, b);
+        }
+
+    }
+}
